ac1341a2a4facbc7bd44d1e69e622cd9c20e0d41
NavodaGunasekera <gimhanigunasekera@gmail.com>
2019-08-10 09:44:49 +0530

    Drinking water reminder

---
+++ app/src/main/java/com/example/myhealthapp/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.example.myhealthapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+    }
+
+}
